16b0d07d11fff9eafcbcc4d83ff85b38ed1cfaa3
Waheed Ahmed <waheedahmed55@gmail.com>
2022-01-20 09:22:33 -0500

    updated the report plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -21,21 +21,6 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
 }
 
-const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');  
-const exec = require('child_process').execSync;  
-module.exports = (on) => {  
-  on('before:run', async (details) => {  
-    console.log('override before:run');  
-    await beforeRunHook(details);  
-    //If you are using other than Windows remove below two lines  
-    await exec("IF EXIST cypress\\screenshots rmdir /Q /S cypress\\screenshots")  
-    await exec("IF EXIST cypress\\reports rmdir /Q /S cypress\\reports")  
-  });
-
-on('after:run', async () => {  
-    console.log('override after:run');  
-    //if you are using other than Windows remove below line starts with await exec  
-    await exec("npx jrm ./cypress/reports/junitreport.xml ./cypress/reports/junit/*.xml");  
-    await afterRunHook();  
-  });  
+module.exports = (on, config) => {
+  require('cypress-mochawesome-reporter/plugin')(on);
 };
